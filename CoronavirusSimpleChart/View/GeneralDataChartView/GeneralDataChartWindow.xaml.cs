5bac55f10de874ed30cc83bc191bf5224d7aab47
renanarnoni <arnoni.r.p@gmail.com>
2020-06-02 16:47:23 -0300

    Adicionar arquivos de projeto.

---
+++ CoronavirusSimpleChart/View/GeneralDataChartView/GeneralDataChartWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CoronavirusSimpleChart.View.GeneralDataChartView
+{
+    /// <summary>
+    /// Lógica interna para GeneralDataChartWindow.xaml
+    /// </summary>
+    public partial class GeneralDataChartWindow : Window
+    {
+        public GeneralDataChartWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CoronavirusSimpleChart/ViewModel/GeneralDataChartViewModel/GeneralDataChartVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoronavirusSimpleChart.ViewModel.GeneralDataChartViewModel
+{
+    class GeneralDataChartVM : INotifyPropertyChanged
+    {
+        public GeneralDataChartVM()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
